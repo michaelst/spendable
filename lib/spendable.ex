5e269d91907b4e9f78c2ab9bd0f1e516d21c6ee4
Michael St Clair <michaelst57@gmail.com>
2019-11-05 21:54:49 -0700

    sort banks by name

--- lib/spendable.ex
+++ lib/spendable.ex
@@ -1,6 +1,13 @@
 defmodule Spendable do
+  alias Spendable.Banks.Account
+  import Ecto.Query, only: [from: 2]
+
   def data() do
     Dataloader.Ecto.new(Spendable.Repo, query: &query/2)
+  end
+
+  def query(Account, _) do
+    from(Account, order_by: :name)
   end
 
   def query(queryable, _params) do
