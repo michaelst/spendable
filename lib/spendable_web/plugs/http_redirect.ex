e2224caa56339051abad8ce8f2dc120e9d688250
Michael St Clair <michaelst57@gmail.com>
2020-02-04 19:19:53 -0700

    log all request headers

--- lib/spendable_web/plugs/http_redirect.ex
+++ lib/spendable_web/plugs/http_redirect.ex
@@ -1,10 +1,13 @@
 defmodule Spendable.Web.HttpRedirect do
   @behaviour Plug
   import Plug.Conn
+  require Logger
 
   def init(opts), do: opts
 
   def call(conn, _) do
+    Logger.info(inspect(conn.req_headers))
+
     conn
     |> get_req_header("http-x-forwarded-proto")
     |> case do
