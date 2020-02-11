897157c014224e9dc7f95fe3823041f1a87f5577
Michael St Clair <michaelst57@gmail.com>
2020-02-10 22:39:39 -0700

    fix eval

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/app/_build/prod/rel/spendable/bin/spendable eval Ecto.Migrator.with_repo(Spendable.Repo, &Ecto.Migrator.run(&1, :up, all: true))
+/app/_build/prod/rel/spendable/bin/spendable eval "Ecto.Migrator.with_repo(Spendable.Repo, &Ecto.Migrator.run(&1, :up, all: true))""
 /app/_build/prod/rel/spendable/bin/spendable start
