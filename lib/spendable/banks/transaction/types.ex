88f7db03675e5afb1fa579a11a85cbf9e5fea518
Michael St Clair <michaelst57@gmail.com>
2020-02-11 17:34:52 -0700

    add pending to bank transaction schema

--- lib/spendable/banks/transaction/types.ex
+++ lib/spendable/banks/transaction/types.ex
@@ -2,9 +2,10 @@ defmodule Spendable.Banks.Transaction.Types do
   use Absinthe.Schema.Notation
 
   object :bank_transaction do
-    field :id, :id
     field :amount, :string
     field :date, :string
+    field :id, :id
     field :name, :string
+    field :pending, :boolean
   end
 end
