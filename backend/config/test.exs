dd4b25eef98479d2af0c889b043b649a8c77948b
Michael St Clair <michaelst57@gmail.com>
2020-05-07 21:24:09 -0600

    setup github actions

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # Configure your database
 config :spendable, Spendable.Repo,
-  password: nil,
+  password: System.get_env("TEST_DB_PASSWORD"),
   database: "spendable_test",
   pool: Ecto.Adapters.SQL.Sandbox
 
