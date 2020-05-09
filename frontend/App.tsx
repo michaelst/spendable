f1622d6388e1091e9b7395d936e3ff5dd5130eab
Michael St Clair <michaelst57@gmail.com>
2020-05-08 19:15:17 -0600

    setup react native for frontend

---
+++ frontend/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3278c8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
