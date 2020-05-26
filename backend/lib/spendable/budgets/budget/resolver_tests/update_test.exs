04f415b2f6692e0587d43bbc44a1c81e44d3c90e
Michael St Clair <michaelst57@gmail.com>
2020-05-26 13:09:51 -0600

    improve test

--- backend/lib/spendable/budgets/budget/resolver_tests/update_test.exs
+++ backend/lib/spendable/budgets/budget/resolver_tests/update_test.exs
@@ -1,16 +1,16 @@
 defmodule Spendable.Budgets.Budget.Resolver.UpdateTest do
-  use Spendable.Web.ConnCase, async: true
+  use Spendable.DataCase, async: true
   import Spendable.Factory
 
   alias Spendable.Repo
 
-  test "update budget", %{conn: conn} do
-    {user, token} = Spendable.TestUtils.create_user()
+  test "update budget" do
+    {user, _token} = Spendable.TestUtils.create_user()
 
     budget = insert(:budget, user: user)
     insert(:allocation, user: user, budget: budget, amount: 10)
 
-    query = """
+    doc = """
       mutation {
         updateBudget(id: #{budget.id}, name: "new name") {
           id
@@ -21,24 +21,19 @@ defmodule Spendable.Budgets.Budget.Resolver.UpdateTest do
       }
     """
 
-    response =
-      conn
-      |> put_req_header("authorization", "Bearer #{token}")
-      |> post("/graphql", %{query: query})
-      |> json_response(200)
+    assert {:ok,
+            %{
+              data: %{
+                "updateBudget" => %{
+                  "id" => "#{budget.id}",
+                  "name" => "new name",
+                  "balance" => "10.00",
+                  "goal" => nil
+                }
+              }
+            }} == Absinthe.run(doc, Spendable.Web.Schema, context: %{current_user: user})
 
-    assert %{
-             "data" => %{
-               "updateBudget" => %{
-                 "id" => "#{budget.id}",
-                 "name" => "new name",
-                 "balance" => "10.00",
-                 "goal" => nil
-               }
-             }
-           } == response
-
-    query = """
+    doc = """
       mutation {
         updateBudget(id: #{budget.id}, name: "new name", balance: "10.05") {
           id
@@ -49,23 +44,43 @@ defmodule Spendable.Budgets.Budget.Resolver.UpdateTest do
       }
     """
 
-    response =
-      conn
-      |> put_req_header("authorization", "Bearer #{token}")
-      |> post("/graphql", %{query: query})
-      |> json_response(200)
-
-    assert %{
-             "data" => %{
-               "updateBudget" => %{
-                 "id" => "#{budget.id}",
-                 "name" => "new name",
-                 "balance" => "10.05",
-                 "goal" => nil
-               }
-             }
-           } == response
+    assert {:ok,
+            %{
+              data: %{
+                "updateBudget" => %{
+                  "id" => "#{budget.id}",
+                  "name" => "new name",
+                  "balance" => "10.05",
+                  "goal" => nil
+                }
+              }
+            }} == Absinthe.run(doc, Spendable.Web.Schema, context: %{current_user: user})
 
     assert Repo.get(Spendable.Budgets.Budget, budget.id).adjustment |> Decimal.eq?("0.05")
+
+    doc = """
+      mutation {
+        updateBudget(id: #{budget.id}, name: "new name", balance: "0.00") {
+          id
+          name
+          balance
+          goal
+        }
+      }
+    """
+
+    assert {:ok,
+            %{
+              data: %{
+                "updateBudget" => %{
+                  "id" => "#{budget.id}",
+                  "name" => "new name",
+                  "balance" => "0.00",
+                  "goal" => nil
+                }
+              }
+            }} == Absinthe.run(doc, Spendable.Web.Schema, context: %{current_user: user})
+
+    assert Repo.get(Spendable.Budgets.Budget, budget.id).adjustment |> Decimal.eq?("-10.00")
   end
 end
