524a4d8db660ca37dd7523f9a606fa39f3ea02ff
Michael St Clair <michaelst57@gmail.com>
2020-05-27 22:31:23 -0600

    fix credo

--- backend/lib/spendable/banks/member/resolver_tests/get_test.exs
+++ backend/lib/spendable/banks/member/resolver_tests/get_test.exs
@@ -5,23 +5,11 @@ defmodule Spendable.Banks.Member.Resolver.GetTest do
   test "get member" do
     {user, _token} = Spendable.TestUtils.create_user()
     member = insert(:bank_member, user: user)
-    checking_account = insert(:bank_account, user: user, bank_member: member, available_balance: 100, balance: 120)
-    savings_account = insert(:bank_account, user: user, bank_member: member, available_balance: nil, balance: 500)
-
-    credit_account = insert(:bank_account, user: user, bank_member: member, balance: 1025, type: "credit")
 
     doc = """
     query {
       bankMember(id: "#{member.id}") {
         id
-        name
-        status
-        bankAccounts {
-          id
-          name
-          sync
-          balance
-        }
       }
     }
     """
@@ -30,14 +18,7 @@ defmodule Spendable.Banks.Member.Resolver.GetTest do
             %{
               data: %{
                 "bankMember" => %{
-                  "bankAccounts" => [
-                    %{"balance" => "-1025.00", "id" => "#{credit_account.id}", "name" => "Checking", "sync" => true},
-                    %{"balance" => "500.00", "id" => "#{savings_account.id}", "name" => "Checking", "sync" => true},
-                    %{"balance" => "100.00", "id" => "#{checking_account.id}", "name" => "Checking", "sync" => true}
-                  ],
-                  "id" => "#{member.id}",
-                  "name" => "Plaid",
-                  "status" => "Connected"
+                  "id" => "#{member.id}"
                 }
               }
             }} == Absinthe.run(doc, Spendable.Web.Schema, context: %{current_user: user})
