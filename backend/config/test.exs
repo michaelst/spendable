3c42165f25c61764251b6076d9c15763afe4a0f2
Michael St Clair <michaelst57@gmail.com>
2020-08-16 18:00:40 -0600

    fix docker build

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -2,10 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :spendable, Spendable.Repo,
-  password: System.get_env("TEST_DB_PASSWORD"),
+  password: System.get_env("TEST_DB_PASSWORD", "postgres"),
   database: "spendable_test",
-  port: "5432",
-  pool_size: "10",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
