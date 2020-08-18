589cba42c5fffc50fa69e9db216a9d1267e68e4e
Michael St Clair <michaelst57@gmail.com>
2020-08-18 17:51:37 -0600

    Secrets (#221)
    * setup releases.exs and better docker builds
    
    * dockerfile improvements
    
    * restore deploy branch

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -8,12 +8,10 @@ config :spendable, Spendable.Web.Endpoint,
     protocol_options: [max_keepalive: :infinity],
     compress: true
   ],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true
 
 config :spendable, Spendable.Repo,
   username: "postgres",
-  password: System.get_env("DB_PASSWORD"),
   database: "spendable",
   hostname: "10.122.112.3",
   pool_size: 5
@@ -24,22 +22,9 @@ config :cors_plug,
   origin: ["https://spendable.money"],
   max_age: 86400
 
-config :spendable, Spendable.Guardian,
-  issuer: "spendable.money",
-  secret_key: System.get_env("GUARDIAN_SECRET")
+config :spendable, Spendable.Guardian, issuer: "spendable.money"
 
-config :pigeon, :apns,
-  apns_default: %{
-    cert: {:spendable, "apns/cert.pem"},
-    key: {:spendable, "apns/key_unencrypted.pem"},
-    mode: :prod
-  }
-
-config :spendable, Plaid,
-  base_url: "https://development.plaid.com",
-  client_id: System.get_env("PLAID_CLIENT_ID"),
-  secret_key: System.get_env("PLAID_SECRET_KEY"),
-  public_key: System.get_env("PLAID_PUBLIC_KEY")
+config :spendable, Plaid, base_url: "https://development.plaid.com"
 
 config :weddell,
   project: "cloud-57"
