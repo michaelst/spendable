3d2dd424372a7382bb76782d20a16b9d512ccc8b
Michael St Clair <michaelst57@gmail.com>
2020-08-18 17:40:07 -0600

    setup releases.exs and better docker builds

---
+++ backend/config/releases.exs
@@ -0,0 +1,25 @@
+import Config
+
+config :spendable, Spendable.Web.Endpoint, secret_key_base: File.read!("/etc/secrets/SECRET_KEY_BASE")
+
+config :spendable, Spendable.Repo, password: File.read!("/etc/secrets/DB_PASSWORD")
+
+config :spendable, Spendable.Guardian, secret_key: File.read!("/etc/secrets/GUARDIAN_SECRET")
+
+config :pigeon, :apns,
+  apns_default: %{
+    cert: File.read!("/etc/secrets/APNS_CERT"),
+    key: File.read!("/etc/secrets/APNS_KEY"),
+    mode: :prod
+  }
+
+config :spendable, Plaid,
+  client_id: File.read!("/etc/secrets/PLAID_CLIENT_ID"),
+  secret_key: File.read!("/etc/secrets/PLAID_SECRET_KEY"),
+  public_key: File.read!("/etc/secrets/PLAID_PUBLIC_KEY")
+
+config :sentry,
+  dsn: System.get_env("SENTRY_DSN")
+
+config :goth,
+  json: File.read!("/etc/secrets/GCP_SA_KEY")
