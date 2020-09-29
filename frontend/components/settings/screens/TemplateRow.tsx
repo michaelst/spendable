72fbe16164c245d013ab258701891ee0614703fc
Michael St Clair <michaelst57@gmail.com>
2020-09-28 22:19:59 -0600

    setup delete template

--- frontend/components/settings/screens/TemplateRow.tsx
+++ frontend/components/settings/screens/TemplateRow.tsx
@@ -8,9 +8,13 @@ import {
 import { useNavigation } from '@react-navigation/native'
 import { useTheme } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons'
-import { ListAllocationTemplates_allocationTemplates } from '../graphql/ListAllocationTemplates'
+import { ListAllocationTemplates, ListAllocationTemplates_allocationTemplates } from '../graphql/ListAllocationTemplates'
 import Decimal from 'decimal.js-light'
 import formatCurrency from 'helpers/formatCurrency'
+import { RectButton } from 'react-native-gesture-handler'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+import { DELETE_TEMPLATE, LIST_TEMPLATES } from '../queries'
+import { useMutation } from '@apollo/client'
 
 type Props = {
   template: ListAllocationTemplates_allocationTemplates,
@@ -22,33 +26,74 @@ export default function TemplateRow({ template }: Props) {
 
   const navigateToTemplate = () => navigation.navigate('Template', { templateId: template.id })
 
+  const [deleteAllocationTemplate] = useMutation(DELETE_TEMPLATE, {
+    variables: { id: template.id },
+    update(cache, { data: { deleteAllocationTemplate } }) {
+      const data = cache.readQuery<ListAllocationTemplates | null>({ query: LIST_TEMPLATES })
+
+      cache.writeQuery({
+        query: LIST_TEMPLATES,
+        data: { allocationTemplates: data?.allocationTemplates.filter(template => template.id !== deleteAllocationTemplate.id) }
+      })
+    }
+  })
+
   const allocated = template.lines.reduce((acc, line) => acc.add(line.amount), new Decimal('0'))
 
   return (
     <TouchableHighlight onPress={navigateToTemplate}>
-      <View
-        style={{
-          flexDirection: 'row',
-          padding: 18,
-          alignItems: 'center',
-          backgroundColor: colors.card,
-          borderBottomColor: colors.border,
-          borderBottomWidth: StyleSheet.hairlineWidth
-        }}
+      <Swipeable
+        renderRightActions={renderRightActions}
+        onSwipeableOpen={deleteAllocationTemplate}
       >
-        <View style={{ flex: 1 }}>
-          <Text style={{ color: colors.text, fontSize: 18 }}>
-            {template.name}
-          </Text>
-        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            padding: 18,
+            alignItems: 'center',
+            backgroundColor: colors.card,
+            borderBottomColor: colors.border,
+            borderBottomWidth: StyleSheet.hairlineWidth
+          }}
+        >
+          <View style={{ flex: 1 }}>
+            <Text style={{ color: colors.text, fontSize: 18 }}>
+              {template.name}
+            </Text>
+          </View>
 
-        <View style={{ flexDirection: "row" }}>
-          <Text style={{ color: colors.secondary, fontSize: 18, paddingRight: 8 }} >
-            {formatCurrency(allocated)}
-          </Text>
-          <Ionicons name='ios-arrow-forward' size={18} color={colors.secondary} />
+          <View style={{ flexDirection: "row" }}>
+            <Text style={{ color: colors.secondary, fontSize: 18, paddingRight: 8 }} >
+              {formatCurrency(allocated)}
+            </Text>
+            <Ionicons name='ios-arrow-forward' size={18} color={colors.secondary} />
+          </View>
         </View>
-      </View>
+      </Swipeable>
     </TouchableHighlight>
+  )
+}
+
+const styles = StyleSheet.create({
+  deleteText: {
+    color: 'white',
+    fontSize: 16,
+    padding: 10,
+    fontWeight: 'bold'
+  },
+  rightAction: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#dd2c00',
+    justifyContent: 'flex-end'
+  },
+})
+
+const renderRightActions = () => {
+  return (
+    <RectButton style={[styles.rightAction, { backgroundColor: '#dd2c00' }]} >
+      <Text style={styles.deleteText}> Delete </Text>
+    </RectButton>
   )
 }
