e982a142d5d220c510df5483d4e98a1df1d2a5db
Michael St Clair <michaelst57@gmail.com>
2021-03-20 12:12:33 -0600

    update frontend

--- frontend/ios/Spendable/AppDelegate.m
+++ frontend/ios/Spendable/AppDelegate.m
@@ -59,7 +59,12 @@
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:self.launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"main" initialProperties:nil];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+  if (@available(iOS 13.0, *)) {
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+    rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
