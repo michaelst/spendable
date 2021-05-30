98016fa827b6cf0c6e1748f384828c0cf6b790ff
Michael St Clair <michaelst57@gmail.com>
2021-05-30 12:00:22 -0600

    add dev id

--- backend/lib/spendable_web/controllers/well_known.ex
+++ backend/lib/spendable_web/controllers/well_known.ex
@@ -4,12 +4,12 @@ defmodule Spendable.Web.Controllers.WellKnown do
   def apple_app_site_association(conn, _params) do
     json(conn, %{
       webcredentials: %{
-        apps: ["A4TA99R8XM.fiftysevenmedia.Spendable"]
+        apps: ["A4TA99R8XM.fiftysevenmedia.Spendable", "A4TA99R8XM.fiftysevenmedia.SpendableDev"]
       },
       appLinks: %{
         apps: [],
         details: [
-          appIDs: ["A4TA99R8XM.fiftysevenmedia.Spendable"],
+          appIDs: ["A4TA99R8XM.fiftysevenmedia.Spendable", "A4TA99R8XM.fiftysevenmedia.SpendableDev"],
           components: [
             %{
               /: "/plaid/oauth.html"
