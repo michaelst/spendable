3a19fcaad276fc7be3a963b74f288aeb08bb55ff
Michael St Clair <michaelst57@gmail.com>
2021-09-25 18:21:51 -0600

    remove changee

--- backend/lib/spendable/resources/notification_settings/changes/create.ex
+++
@@ -1,9 +0,0 @@
-defmodule Spendable.NotificationSettings.Changes.Create do
-  use Ash.Resource.Change
-
-  def change(changeset, _opts, _context) do
-    Ash.Changeset.after_action(changeset, fn _changeset, record ->
-      IO.inspect(record)
-    end)
-  end
-end
