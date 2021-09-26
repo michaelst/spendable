49a64eff9c47400873054d2d15b1f1544f0411dc
Michael St Clair <michaelst57@gmail.com>
2021-09-25 20:13:08 -0600

    remove middleware

--- backend/lib/middleware/check_authentication.ex
+++
@@ -1,8 +0,0 @@
-defmodule Spendable.Middleware.CheckAuthentication do
-  @behaviour Absinthe.Middleware
-
-  alias Absinthe.Resolution
-
-  def call(%{context: %{current_user: _}} = resolution, _config), do: resolution
-  def call(resolution, _config), do: resolution |> Resolution.put_result({:error, "unauthenticated"})
-end
