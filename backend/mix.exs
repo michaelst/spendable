be560877ce2db89ca2ef1bcfe58ca6c8770554c5
Michael St Clair <michaelst57@gmail.com>
2022-04-23 19:09:33 -0600

    0.1.1

--- backend/mix.exs
+++ backend/mix.exs
@@ -4,7 +4,7 @@ defmodule Spendable.MixProject do
   def project() do
     [
       app: :spendable,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
