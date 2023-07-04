392a925e36c8537154df32c59e98abcbfb737716
Michael St Clair <michaelst57@gmail.com>
2023-07-04 15:56:09 -0600

    combine into main folder

---
+++ lib/spendable/resources/budget/storage.ex
@@ -0,0 +1,47 @@
+defmodule Spendable.Budget.Storage do
+  alias Spendable.Api
+  alias Spendable.Budget
+
+  require Ash.Query
+
+  def list(user_id, opts \\ []) do
+    Budget
+    |> Ash.Query.filter(user_id == ^user_id)
+    |> Ash.Query.filter(is_nil(archived_at))
+    |> maybe_search_budgets(opts[:search])
+    |> Ash.Query.load([
+      :balance,
+      spent: %{month: opts[:selected_month] || Date.utc_today()}
+    ])
+    |> Ash.Query.after_action(fn _query, results ->
+      {:ok,
+       Enum.sort(results, fn a, b ->
+         to_string(b.name) != "Spendable" and
+           (to_string(a.name) == "Spendable" or a.name < b.name)
+       end)}
+    end)
+    |> Api.read!()
+  end
+
+  def form_options(user_id) do
+    Budget
+    |> Ash.Query.filter(user_id == ^user_id)
+    |> Ash.Query.filter(is_nil(archived_at))
+    |> Ash.Query.select([:id, :name])
+    |> Ash.Query.after_action(fn _query, results ->
+      {:ok,
+       Enum.sort(results, fn a, b ->
+         to_string(b.name) != "Spendable" and
+           (to_string(a.name) == "Spendable" or a.name < b.name)
+       end)}
+    end)
+    |> Api.read!()
+    |> Enum.map(&{&1.name, &1.id})
+  end
+
+  defp maybe_search_budgets(query, search) when byte_size(search) > 0 do
+    Ash.Query.filter(query, contains(name, ^search))
+  end
+
+  defp maybe_search_budgets(query, _search), do: query
+end
---
+++ lib/spendable/resources/user.ex
@@ -0,0 +1,45 @@
+defmodule Spendable.User do
+  use Ash.Resource,
+    authorizers: [Ash.Policy.Authorizer],
+    data_layer: AshPostgres.DataLayer
+
+  alias Spendable.User.SpentByMonth
+
+  postgres do
+    repo(Spendable.Repo)
+    table "users"
+  end
+
+  attributes do
+    integer_primary_key :id
+
+    attribute :bank_limit, :integer, default: 0, allow_nil?: false
+    attribute :external_id, :string, allow_nil?: false
+    attribute :image, :string
+    attribute :provider, :string, allow_nil?: false
+
+    timestamps()
+  end
+
+  identities do
+    identity :external_id, [:external_id]
+  end
+
+  calculations do
+    calculate :plaid_link_token, :string, Spendable.User.Calculations.PlaidLinkToken, allow_nil?: false
+
+    calculate :spendable, :decimal, Spendable.User.Calculations.Spendable, allow_nil?: false
+
+    calculate :spent_by_month, {:array, SpentByMonth}, Spendable.User.Calculations.SpentByMonth, allow_nil?: false
+  end
+
+  actions do
+    defaults [:read, :create]
+  end
+
+  policies do
+    policy always() do
+      authorize_if expr(id == ^actor(:id))
+    end
+  end
+end
