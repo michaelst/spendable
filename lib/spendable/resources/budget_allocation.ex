54b2780aa9d6e503b2b69648e4f6f1cb699237dd
Michael St Clair <michaelst57@gmail.com>
2023-07-05 11:15:19 -0600

    allocation uuid

--- lib/spendable/resources/budget_allocation.ex
+++ lib/spendable/resources/budget_allocation.ex
@@ -8,25 +8,14 @@ defmodule Spendable.BudgetAllocation do
     table "budget_allocations"
 
     custom_indexes do
-      # index(["budget_id"])
-      # index(["transaction_id"])
-      # index(["user_id"])
+      index(["budget_id"])
+      index(["transaction_id"])
+      index(["user_id"])
     end
-  end
-
-  identities do
-    identity :uuid, [:uuid]
   end
 
   attributes do
-    integer_primary_key :id
-
-    attribute :uuid, :uuid do
-      writable? false
-      default &Ash.UUID.generate/0
-      primary_key? false
-      generated? true
-    end
+    uuid_primary_key :id
 
     attribute :amount, :decimal, allow_nil?: false
 
@@ -35,9 +24,7 @@ defmodule Spendable.BudgetAllocation do
 
   relationships do
     belongs_to :transaction, Spendable.Transaction, allow_nil?: false, attribute_type: :integer
-
     belongs_to :budget, Spendable.Budget, allow_nil?: false
-
     belongs_to :user, Spendable.User, allow_nil?: false
   end
 
