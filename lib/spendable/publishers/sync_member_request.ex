1767b6f98b1019a4a80594c3a105fb462eb4e9e0
Michael St Clair <michaelst57@gmail.com>
2023-07-05 12:25:01 -0600

    fix protos (#758)

--- lib/spendable/publishers/sync_member_request.ex
+++ lib/spendable/publishers/sync_member_request.ex
@@ -9,7 +9,7 @@ defmodule Spendable.Publishers.SyncMemberRequest do
 
   defp pubsub(), do: Application.get_env(:mox, :pubsub, PubSub)
 
-  def publish(member_id) when is_integer(member_id) do
+  def publish(member_id) when is_binary(member_id) do
     %Banks.V1.SyncMemberRequest{member_id: member_id}
     |> Banks.V1.SyncMemberRequest.encode()
     |> pubsub().publish(@topic)
