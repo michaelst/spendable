bc1deee842abd63c76b88a9bf3a6381f13af8ccf
Michael St Clair <michaelst57@gmail.com>
2024-04-07 15:23:32 -0600

    ignore budgets connected to bank for spendable

--- lib/spendable/resources/user/calculations/spendable.ex
+++ lib/spendable/resources/user/calculations/spendable.ex
@@ -33,10 +33,14 @@ defmodule Spendable.User.Calculations.Spendable do
       from(a in subquery(allocations_query),
         full_join: b in Budget,
         on: a.budget_id == b.id,
+        left_join: ba in BankAccount,
+        on: b.id == ba.budget_id,
         select: fragment("SUM(ABS(COALESCE(?, 0) + ?))", a.allocated, b.adjustment),
         where: b.user_id == ^user.id,
         # ignore budgets that are only used to track spending
-        where: b.type != :tracking
+        where: b.type != :tracking,
+        # ignore budgets that are allocated from a bank account balance
+        where: is_nil(ba.id)
       )
       |> Repo.one()
       |> Kernel.||("0.00")
